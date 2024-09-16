56c0500589b5f60e01a44e0cea6d4039c26753e6
sabyasachimohapatra <mohapatrasabyasachi162004@gmail.com>
2024-09-16 18:28:01 +0530

    Time: 4 ms (14.52%), Space: 8.70 MB (76.26%)

---
+++ implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -0,0 +1,17 @@
+class MyQueue {
+public:
+    stack<int>son1,son2;
+    MyQueue(){}
+    void push(int x) {
+      while(!son1.empty()){
+        son2.push(son1.top());
+        son1.pop();}
+      son1.push(x);
+      while(!son2.empty()){
+        son1.push(son2.top());
+        son2.pop();}}
+    int pop() {
+      int temp=son1.top();
+      son1.pop();return temp;}
+    int peek() {return son1.top();}
+    bool empty() {return son1.empty();}};
