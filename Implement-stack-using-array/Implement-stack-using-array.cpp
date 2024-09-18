10974fae3e028ea17fa8d47be5321bebab46696d
sabyasachimohapatra <mohapatrasabyasachi162004@gmail.com>
2024-09-18 17:46:17 +0530

    Create Implement-stack-using-array.cpp

---
+++ Implement-stack-using-array/Implement-stack-using-array.cpp
@@ -0,0 +1,17 @@
+
+void MyStack ::push(int x) {
+    // Your Code
+    top++;
+    arr[top] = x;
+}
+
+// Function to remove an item from top of the stack.
+int MyStack ::pop() {
+    // Your Code
+    if (top == -1) {
+        return -1;  // Return -1 if stack is empty
+    }
+    int x = arr[top];
+    top--;
+    return x;
+}
